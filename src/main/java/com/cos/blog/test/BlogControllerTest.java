d4fa61354ff633bea2f8b65ba87f4d0ebffe0512
119wjw <119wjw@naver.com>
2021-06-30 23:23:37 +0900

    feat : ControllerTest

--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -1,15 +1,42 @@
 package com.cos.blog.test;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //스프링이 com.cos.blog 패키지 이하를 스캔해서 모든 파일을 메모리에 new하는 것은 아니고
 //특정 어노테이션이 붙어있는 클래스 파일들을 new해서(IoC) 스프링 컨테이너에 관리해준다.
 @RestController
 public class BlogControllerTest {
 
-    @GetMapping("/test/hello")
-    public String hello(){
-        return "<h1>hello spring boot</h1>";
+    private static final String TAG = "HttpControllerTest : ";
+
+    @GetMapping("/http/lombok")
+    public String lombokTest(){
+        Member m = Member.builder().username("ssar").password("1234").email("email@nate.com").build();
+        System.out.println(TAG + "getter" + m.getId());
+        m.setId(5000);
+        System.out.println(TAG + "setter" + m.getId());
+        return "lombok test 완료";
+    }
+
+    //http://localhost:8080/http/get (select)
+    @GetMapping("/http/get")
+    public String getTest(Member m){
+        return "get 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    }
+
+    //http://localhost:8080/http/post (insert)
+    @PostMapping("/http/post")
+    public String postTest(Member m){
+        return "post 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    }
+    //http://localhost:8080/http/put (update)
+    @PutMapping("/http/put")
+    public String putTest(){
+        return "put 요청";
+    }
+    //http://localhost:8080/http/delete (delete)
+    @DeleteMapping("/http/delete")
+    public String deleteTest(){
+        return "delete 요청";
     }
 }
