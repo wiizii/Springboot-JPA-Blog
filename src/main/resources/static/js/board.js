deaf40ed3d7164d65169f383e6ab84ea3c033fa4
119wjw <119wjw@naver.com>
2021-07-01 14:59:51 +0900

    feat : javascript file for Ajax
    - use jquery

---
+++ src/main/resources/static/js/board.js
@@ -0,0 +1,104 @@
+let index = {
+    init: function(){
+        $("#btn-save").on("click", () => { // function(){}, ()=>{} this를 바인딩하기 위해서!
+            this.save();
+        });
+        $("#btn-delete").on("click", () => { // function(){}, ()=>{} this를 바인딩하기 위해서!
+            this.deleteById();
+        });
+        $("#btn-update").on("click", () => { // function(){}, ()=>{} this를 바인딩하기 위해서!
+            this.update();
+        });
+        $("#btn-reply-save").on("click", () => { // function(){}, ()=>{} this를 바인딩하기 위해서!
+            this.replySave();
+        });
+    },
+
+    save: function(){
+        //alert('user의 save함수 호출됨');
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val(),
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/api/board",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function(res){
+            alert("글쓰기가 완료되었습니다.");
+            location.href="/";
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    },
+    deleteById: function(){
+        let id = $("#id").text();
+        console.log(id)
+        $.ajax({
+            type: "DELETE",
+            url: "/api/board/"+id,
+            dataType: "json"
+        }).done(function(res){
+            alert("삭제가 완료되었습니다.");
+            location.href="/";
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    },
+    update: function(){
+        let id = $("#id").val();
+
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val(),
+        };
+
+        $.ajax({
+            type: "PUT",
+            url: "/api/board/"+id,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function(res){
+            alert("글수정이 완료되었습니다.");
+            location.href="/";
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    },
+    replySave: function(){
+        let data = {
+            content: $("#reply-content").val(),
+        };
+        let boardId = $("#boardId").val();
+        $.ajax({
+            type: "POST",
+            url: `/api/board/${boardId}/reply`,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function(res){
+            alert("댓글작성이 완료되었습니다.");
+            location.href=`/board/${boardId}`;
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    },
+    replyDelete: function(boardId, replyId){
+        $.ajax({
+            type: "DELETE",
+            url: `/api/board/${boardId}/reply/${replyId}`,
+            dataType: "json"
+        }).done(function(res){
+            alert("댓글 삭제 성공");
+            location.href=`/board/${boardId}`;
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    },
+}
+
+index.init();
